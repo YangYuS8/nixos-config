6b025df53ec1d393d67d3d8178342e0ca977b147
YangYuS8 <YangYuS8@163.com>
2025-11-08 15:20:00 +0800

    fix: 修正 Fcitx5 插件引用，确保正确加载中文输入法

--- chinese.nix
+++ chinese.nix
@@ -22,7 +22,7 @@
     enable = true;
     type = "fcitx5";
     fcitx5.addons = with pkgs; [
-    fcitx5-chinese-addons
+    qt6Packages.fcitx5-chinese-addons
    ];
    fcitx5.waylandFrontend = true;
   };
